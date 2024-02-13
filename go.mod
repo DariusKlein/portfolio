9525329c8a2e1698eb94b8d1918a24ec02a0362e
darius <darius.klein@student.hu.nl>
2024-02-14 00:08:14 +0100

    Ent orm added Error handler split file Git ignore added temporary test endpoints added

--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module portfolio-backend
 
 go 1.22
+
+require (
+	entgo.io/ent v0.13.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	ariga.io/atlas v0.19.1-0.20240203083654-5948b60a8e43 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.19.1 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/zclconf/go-cty v1.14.2 // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -8,14 +8,36 @@ func main() {
 	// Take incoming requests and dispatch them to the matching handlers
 	mux := http.NewServeMux()
 
+	//connect to database and migrate
+	DB()
+
 	// Register the routes and handlers
-	mux.HandleFunc("GET /", test)
+	mux.HandleFunc("/", catchAllHandler)
+	mux.HandleFunc("/test", testHandler)
+	mux.HandleFunc("/test2", test2Handler)
 
 	// Run the server
 	http.ListenAndServe(":4002", mux)
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	println("test123")
-	w.Write([]byte("test"))
+func catchAllHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusGone)
+	_, err := w.Write([]byte("Bad endpoint"))
+	if err != nil {
+		InternalServerErrorHandler(w, r)
+	}
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("test"))
+	if err != nil {
+		InternalServerErrorHandler(w, r)
+	}
+}
+
+func test2Handler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("test2"))
+	if err != nil {
+		InternalServerErrorHandler(w, r)
+	}
 }
