4d12a642a253bf5ae22eb413cde13095a545b9ee
darius <darius.klein@student.hu.nl>
2024-03-14 19:24:41 +0100

    bootstrap navbar added

--- api/webHandler/homepageHandler.go
+++ api/webHandler/homepageHandler.go
@@ -3,22 +3,33 @@ package webHandler
 import (
 	"html/template"
 	"net/http"
-	"portfolio/types"
 )
 
 func InitHomepage(w http.ResponseWriter, r *http.Request) {
 
-	tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+	tmpl := template.Must(template.ParseFiles(
+		"./templates/homepage/index.html",
+		"./templates/navbar/index.html",
+	))
 
-	userNames := map[string][]types.Username{
-		"Names": {
-			{Name: "The Godfather"},
-			{Name: "Blade Runner"},
-			{Name: "The Thing"},
-		},
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		_, err := w.Write([]byte("failed to load"))
+		if err != nil {
+			return
+		}
+		return
 	}
+}
 
-	err := tmpl.Execute(w, userNames)
+func Test(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles(
+		"./templates/homepage/index.html",
+		"./templates/navbar/index.html",
+	))
+
+	err := tmpl.Execute(w, nil)
 	if err != nil {
 		_, err := w.Write([]byte("failed to load"))
 		if err != nil {
--- api/webRoutes.go
+++ api/webRoutes.go
@@ -9,4 +9,9 @@ func WebRoutes(mux **http.ServeMux) {
 	m := *mux
 	// Register the routes and webHandler
 	m.HandleFunc("GET /{$}", webHandler.InitHomepage)
+	m.HandleFunc("GET /projecten/{$}", webHandler.InitProjectpage)
+
+	m.HandleFunc("GET /test", webHandler.Test)
+
+	m.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./templates/assets"))))
 }
