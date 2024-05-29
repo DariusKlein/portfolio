a414a7b606c5c65445b775e43fc3b2af4bcc09e3
darius <darius.klein@student.hu.nl>
2024-05-29 16:14:34 +0200

    foto added

--- web/handlers/homePageHandler.go
+++ web/handlers/homePageHandler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	g "github.com/maragudk/gomponents"
 	. "github.com/maragudk/gomponents/html"
+	b "github.com/willoma/bulma-gomponents"
 	"net/http"
 )
 
@@ -17,5 +18,12 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 
 func createHomeBody(w http.ResponseWriter, r *http.Request) g.Node {
 
-	return Body()
+	return Body(
+		Div(
+			Class("flex max-h-[calc(100vh_-_3.75rem)]"),
+			b.ImageImg("assets/images/Darius_pasfoto_2023_Large.png",
+				b.OnImg(Class("object-scale-down max-h-full m-auto")),
+			)),
+		Hr(Class("h-1 border-t-0 bg-neutral-300 dark:bg-white/100 flex")),
+	)
 }
