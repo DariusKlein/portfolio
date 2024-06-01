3d4f6983e7b828ba82117d7cd2194c55cc44864b
darius <darius.klein@student.hu.nl>
2024-06-01 18:19:04 +0200

    home page build

--- web/handlers/homePageHandler.go
+++ web/handlers/homePageHandler.go
@@ -4,6 +4,7 @@ import (
 	g "github.com/maragudk/gomponents"
 	. "github.com/maragudk/gomponents/html"
 	b "github.com/willoma/bulma-gomponents"
+	e "github.com/willoma/gomplements"
 	"net/http"
 )
 
@@ -20,10 +21,24 @@ func createHomeBody(w http.ResponseWriter, r *http.Request) g.Node {
 
 	return Body(
 		Div(
-			Class("flex max-h-[calc(100vh_-_3.75rem)]"),
-			b.ImageImg("assets/images/Darius_pasfoto_2023_Large.png",
-				b.OnImg(Class("object-scale-down max-h-full m-auto")),
-			)),
+			b.Media(
+				b.MarginBottom(0),
+				b.MediaLeft(
+					Figure,
+					b.ImageImg("assets/images/Darius_pasfoto_2023_Large.png",
+						b.OnImg(Class("object-scale-down max-h-[calc(100vh_-_3.75rem)]")),
+					),
+				),
+			),
+			b.Section(
+				e.H1("Darius Klein",
+					Class("title backdrop-blur")),
+				e.H2(
+					"Backend developer die zelf zijn servers kan opzetten.",
+					Class("subtitle backdrop-blur")),
+			),
+			Class("flex"),
+		),
 		Hr(Class("h-1 border-t-0 bg-neutral-300 dark:bg-white/100 flex")),
 	)
 }
